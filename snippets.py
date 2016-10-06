1c5c917cfe2e13dd8da5d7163d02c72c351a617e
Francis Alano <francis.alano@gmail.com>
2016-10-06 02:00:18 +0000

    Initial commit

---
+++ snippets.py
@@ -0,0 +1,19 @@
+import logging
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+
+put("list", "A sequence of things - created using []")
+get("list")
